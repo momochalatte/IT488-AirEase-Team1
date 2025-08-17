15fb1b52b0002045e713b7d5c993796164e3f061
loganbowlingpg <loganbowling@student.purdueglobal.edu>
2025-08-16 21:34:56 -0400

    Add files via upload
    Reusable form input and button components.

---
+++ IT488_AirEase_Team1/Controls/button.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Util;
+using Android.Widget;
+
+namespace IT488_AirEase_Team1.Controls
+{
+    public class PrimaryButton : Button
+    {
+        public PrimaryButton(Context context) : base(context)
+        {
+            Init();
+        }
+
+        public PrimaryButton(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            SetBackgroundColor(Color.ParseColor("#007BFF"));
+            SetTextColor(Color.White);
+            SetPadding(40, 20, 40, 20);
+            Typeface = Typeface.DefaultBold;
+            TextSize = 18;
+        }
+    }
+}
---
+++ IT488_AirEase_Team1/Controls/form_entry.cs
@@ -0,0 +1,52 @@
+﻿
+using Android.Content;
+using Android.Util;
+using Android.Views;
+
+
+
+namespace IT488_AirEase_Team1.Controls
+{
+    public class FormEntry : LinearLayout
+    {
+        private TextView _label = null!;
+        private EditText _input = null!;
+        
+        public FormEntry(Context context) : base(context)
+        {
+            Init(context);
+        
+        }
+
+        public FormEntry(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Init(context);
+        }
+
+        private void Init(Context context)
+        {
+            
+            var view = LayoutInflater.From(context)!.Inflate(Resource.Layout.form_entry, this, true);
+            _label = view.FindViewById<TextView>(Resource.Id.label)!;
+            _input = view.FindViewById<EditText>(Resource.Id.input)!;
+        }
+
+        public string Title
+        {
+            get => _label!.Text!;
+            set => _label!.Text = value;
+        }
+
+        public string Text
+        {
+            get => _input!.Text!;
+            set => _input!.Text = value;
+        }
+
+        public string Placeholder
+        {
+            get => _input!.Hint!;
+            set => _input!.Hint = value;
+        }
+    }
+}
